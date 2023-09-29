13a11ad8043d4ca1d371a6c21cd7ba35c0e461bc
GaiaMok <sylvia.mokoka@gmail.com>
2023-09-29 16:01:58 -0700

    my library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: function parameter
+ * Return: 0 or 1 on success
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
